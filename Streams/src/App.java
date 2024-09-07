b267ccf1b62be24296ad135b79071a2a7e02413c
Shellbaka <shellbaka69@gmail.com>
2024-09-07 11:42:39 -0300

    Testando o primeiro uso de Streams fazendo uma ForEach para cada número par ser imprimido de uma lista de arrays.

--- Streams/src/App.java
+++ Streams/src/App.java
@@ -1,5 +1,26 @@
+import java.util.List;
+import java.util.function.Consumer;
+
+import java.util.Arrays;
+
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5);
+
+        Consumer<Integer> imprimirNumeroPar = numero -> {
+        if(numero % 2 == 0) {
+            System.out.println(numero + " ");
+        }
+        };
+
+        numeros.stream().forEach(imprimirNumeroPar);
+
     }
+
+
+
+
+
 }
